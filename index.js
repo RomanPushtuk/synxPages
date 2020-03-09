57a3423594ea594665a7c62a9a91a016f607facd
Roman Pushtuk <r123321456@gmail.com>
2020-03-09 14:05:12 +0300

    Синхронизация страниц

---
+++ index.js
@@ -0,0 +1,20 @@
+let globalHref = null;
+
+window.addEventListener("focus", () => {
+    if(globalHref) {
+        setTimeout(() => window.location = globalHref, 300);
+    }
+});
+
+document.body.addEventListener("click", event => {
+    if (event.target.tagName === "A"){
+        if(localStorage.getItem("HREF")){
+            localStorage.removeItem("HREF")
+        }
+        localStorage.setItem("HREF", event.target.href);
+    }
+});
+
+window.addEventListener("storage", event =>  {
+    globalHref = event.newValue;
+});
